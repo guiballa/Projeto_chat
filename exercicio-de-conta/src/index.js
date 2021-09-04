7ec0c3b8f834e49d671a3fa6d16c034d682e80bd
Guiballa <31780336+guiballa@users.noreply.github.com>
2021-09-04 13:00:24 -0300

    Começo de exercicio de contas

---
+++ exercicio-de-conta/src/index.js
@@ -0,0 +1,24 @@
+import 'primereact/resources/themes/saga-blue/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+
+import React, { Component } from 'react'
+//import 'bootstrap/dist/css/bootstrap.min.css'
+import ReactDOM from 'react-dom'
+import Jogo from './Jogo'
+
+
+
+export default class App extends Component {
+    render() {
+        return (
+            <Jogo/>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
